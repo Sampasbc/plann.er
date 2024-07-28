692389924a21fd47fba3864eaf7b3807926d800a
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-07-28 15:14:00 +0100

    update environmental variables on client

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const apiURL = import.meta.env.VITE_API_URL;
+const apiPORT = import.meta.env.VITE_API_PORT;
 
 export const api = axios.create({
-  baseURL: `${apiURL}:3333`
+  baseURL: `${apiURL}:${apiPORT}`
 })
 
