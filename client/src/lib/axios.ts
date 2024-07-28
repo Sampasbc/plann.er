75dd8b949a13447fff162f6a944693c547cb92eb
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-07-28 15:59:24 +0100

    update baseURL for api connections

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -4,6 +4,6 @@ const apiURL = import.meta.env.VITE_API_URL;
 const apiPORT = import.meta.env.VITE_API_PORT;
 
 export const api = axios.create({
-  baseURL: `${apiURL}:${apiPORT}`
+  baseURL: `${apiURL}${apiPORT}`
 })
 
