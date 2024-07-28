39ff534ee9a7c8372dbf746b2e0b1bc3db78796b
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-07-28 16:22:59 +0100

    change environmental variables

--- server/src/env.ts
+++ server/src/env.ts
@@ -4,9 +4,11 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   API_BASE_URL: z.string().url(),
   API_BASE_URL_PROTOCOL: z.string(),
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.string(),
+  // PORT: z.coerce.number().default(3333),
   CLIENT_BASE_URL: z.string().url(),
-  CLIENT_PORT: z.coerce.number().default(5555),
+  CLIENT_PORT: z.coerce.string(),
+  // CLIENT_PORT: z.coerce.number().default(5555),
 })
 
 export const env = envSchema.parse(process.env)
